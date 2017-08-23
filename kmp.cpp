c172516a2eeeb88c05b645c4fbcff33a8fb9b913
Luffbee <liuyifan@tiaozhan.com>
2017-08-23 22:05:05 +0800

    update kmp

--- kmp.cpp
+++ kmp.cpp
@@ -1,5 +1,5 @@
-#define FOR(i, n) for (int (i) = 0; (i) < (n); (i)++)
-#define FOR1(i, n) for (int (i) = 1; (i) <= (n); (i)++)
+#define FOR(i, n) for (int (i) = 0; (i) < (n); ++(i))
+#define FOR1(i, n) for (int (i) = 1; (i) <= (n); ++(i))
 
 using namespace std;
 
@@ -11,19 +11,23 @@ const int MAXN = 1e6;
 int fail[MAXN+1];	
 void makefail(char * s) {
 	fail[0] = 0;
-	for (int i = 1, j = 0; s[i]; i++) {
+	for (int i = 1, j = 0; s[i]; ++i) {
 		while (s[i] != s[j] && j != 0) j = fail[j-1];
-		if (s[i] == s[j]) j++;
+		if (s[i] == s[j]) ++j;
 		fail[i] = j;
 	}
 }
 
 int kmp(char * str, char * s) {
-	int ret = 0;
-	for (int i = 0, j = 0; str[i]; i++) {
+	int ret = 0, i, j;
+	for (i = 0, j = 0; str[i]; ++i) {
 		while (str[i] != s[j] && j != 0) j = fail[j-1];
-		if (str[i] == s[j]) j++;
-		if (!s[j]) {ret++; j = 0;}
+		if (str[i] == s[j]) ++j;
+		if (!s[j]) {
+			++ret;
+			j = 0; // no overlay
+			// j = fail[j-1]; // overlay
+		}
 	}
 	return ret;
 }
