4f58be8f5c4953e674fbde8abcdab954011f92a2
Luffbee <liuyifan@tiaozhan.com>
2017-08-23 20:57:52 +0800

    init

---
+++ kmp.cpp
@@ -0,0 +1,33 @@
+#define FOR(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR1(i, n) for (int (i) = 1; (i) <= (n); (i)++)
+
+using namespace std;
+
+const int MAXN = 1e6;
+
+// str:		text
+// s:		pattern
+// MAXN:	max length
+int fail[MAXN+1];	
+void makefail(char * s) {
+	fail[0] = 0;
+	for (int i = 1, j = 0; s[i]; i++) {
+		while (s[i] != s[j] && j != 0) j = fail[j-1];
+		if (s[i] == s[j]) j++;
+		fail[i] = j;
+	}
+}
+
+int kmp(char * str, char * s) {
+	int ret = 0;
+	for (int i = 0, j = 0; str[i]; i++) {
+		while (str[i] != s[j] && j != 0) j = fail[j-1];
+		if (str[i] == s[j]) j++;
+		if (!s[j]) {ret++; j = 0;}
+	}
+	return ret;
+}
+
+int main() {
+	return 0;
+}
