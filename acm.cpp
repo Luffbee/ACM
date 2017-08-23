4efa5b957cac657f310f65e5a6d806daab53aa35
Luffbee <liuyifan@tiaozhan.com>
2017-08-23 21:00:12 +0800

    add acm.cpp

---
+++ acm.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR1(i, n) for (int (i) = 1; (i) <= (n); (i)++)
+
+using namespace std;
+
+typedef long long LL;
+
+const int MAXN = 20;
+
+int main() {
+	return 0;
+}
