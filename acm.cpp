6c8ac136d4f1a49cb625c4e443aebc35ca79ae69
Luffbee <liuyifan@tiaozhan.com>
2017-09-10 01:45:26 +0800

    add fft

--- acm.cpp
+++ acm.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#define FOR(i, n) for (int (i) = 0; (i) < (n); (i)++)
-#define FOR1(i, n) for (int (i) = 1; (i) <= (n); (i)++)
+#define FOR(i, n) for (int (i) = 0; (i) < (n); ++(i))
+#define FOR1(i, n) for (int (i) = 1; (i) <= (n); ++(i))
 
 using namespace std;
 
